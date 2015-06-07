f86f51ac617289091807540f34c4cd00fb3e9cf8
Snake <eric.unnervik@epfl.ch>
2015-06-07 22:27:36 +0200

    gohome

--- objectdetection/src/Robot.h
+++ objectdetection/src/Robot.h
@@ -12,33 +12,36 @@
 enum Wheels{LEFT, RIGHT};
 enum ServoState{UP, DOWN};
 enum Sensor{IRL, IRFL, IRFR, IRR, IRB, USL, USFL, USFR, USR};
+
 struct Pose
 {
-int x;
-int y;
-int angle;
+	int x;
+	int y;
+	int angle;
 };
+
 class Robot {
 public:
-Robot(Brain* brain);
-virtual ~Robot();
-void updateData();
-void sendInstructions(){if (m_brain != NULL) m_brain->sendInstructions();};
-void setWheelSpeeds(int left, int right);
-void setTailGate(int tailGateState);
-void setShovel(int shovelState);
-void setBrushSpeed(int speed);
-int getSensorValue(char sensorId);
-//int getBrushCurrent();
-int getBottleAngle();
-int getBottleDistance();
-Pose getPose();
-void changeTileState(int x, int y, Tile newTile); // adds values to the map
-Tile getTile(int x, int y); //returns tile contents
-//TODO : stuff with camera ? for example int getBottleDirection
-void printMap();
+	Robot(Brain* brain);
+	virtual ~Robot();
+	void updateData();
+	void sendInstructions(){if (m_brain != NULL) m_brain->sendInstructions();};
+	void setWheelSpeeds(int left, int right);
+	void setTailGate(int tailGateState);
+	void setShovel(int shovelState);
+	void setBrushSpeed(int speed);
+	int getSensorValue(char sensorId);
+	//int getBrushCurrent();
+	int getBottleAngle();
+	int getBottleDistance();
+	Pose getPose();
+	void changeTileState(int x, int y, Tile newTile); // adds values to the map
+	Tile getTile(int x, int y); //returns tile contents
+	//TODO : stuff with camera ? for example int getBottleDirection
+	void printMap();
+
 private:
-Brain* m_brain;
-Map* m_map;
+	Brain* m_brain;
+	Map* m_map;
 };
 #endif /* ROBOT_H_ */
